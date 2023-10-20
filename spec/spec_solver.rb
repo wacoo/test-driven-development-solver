a85bc9303f456417933a05c437db52122c3f2914
wac <wabaham9@gmail.com>
2023-10-20 20:31:24 +0300

    Add linters config

--- spec/spec_solver.rb
+++
@@ -1,8 +0,0 @@
-describe Solver do
-    context 'solver class' do
-        it '(solver) should be an insance of Solver class' do
-            solver = Solver.new
-            expect(solver).to be_instance_of(Sover)
-        end
-    end
-end
