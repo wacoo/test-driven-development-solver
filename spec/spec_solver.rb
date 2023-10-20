faa065a4790c9318029eb8d51559553429144728
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-10-21 00:06:27 +0100

    add some new spec cases

--- spec/spec_solver.rb
+++ spec/spec_solver.rb
@@ -40,7 +40,7 @@ describe Solver do
         it '(reverse("book") should return koob' do
             input = 'book'
             output = 'koob'
-            solver = Solver.new 
+            solver = Solver.new
             expect(solver.reverse(input)).to eql(output)
         end
     end
