0ef28ad05c9dd16a9feea09d21de2648fb63f15e
wac <wabaham9@gmail.com>
2023-10-21 11:27:55 +0300

    Add Solver class and methods

---
+++ classes/solver.rb
@@ -0,0 +1,43 @@
+class Solver
+    def factorial(n)
+        num = 1
+        total = 1
+        if n < 0
+            puts raise('NegativeNumber')
+        elsif n == 0
+            1
+        else
+            while (num <= n)
+                total *= num
+                num += 1
+            end
+            total
+        end
+    end
+
+    def reverse(str)
+        strArray = str.chars
+        reversedArray = strArray.reverse
+        reversed = reversedArray.join
+        reversed
+    end
+
+    def fizzbuzz(n)
+        if n < 1
+            "#{n}"
+        else
+            if n % 3 == 0 && n % 5 == 0
+                'fizzbuzz'
+            elsif n % 3 == 0
+                'fizz'
+            elsif n % 5 == 0
+                'buzz'
+            else
+                "#{n}"
+            end
+        end
+    end
+end
+
+# sol = Solver.new
+# puts sol.fizzbuzz(0)
